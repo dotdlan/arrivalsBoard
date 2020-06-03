43380dad30a60e7188c2450445681f92e1d46fb3
David Lance <david6116@yahoo.com>
2020-06-03 11:54:04 -0400

    initial commit for flightboard

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const app = express()
+const db = mongoose.connection
+require('dotenv').config()
+// PORT
+const PORT = process.env.PORT || 3333
+// Database
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+)
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+// middleware
+// use the public folder
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(PORT, () => {
+    console.log('Listening on port:', PORT)
+})
