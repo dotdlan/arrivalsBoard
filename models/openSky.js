dc5d632b08b7ce1b7f7801184c15e5b061a4f5dd
David Lance <david6116@yahoo.com>
2020-06-07 11:38:05 -0400

    first pass at writing a seeder a model of opensky data

--- models/openSky.js
+++ models/openSky.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const flightSchema = new mongoose.Schema({})
+const flightSchema = new mongoose.Schema({
+    flightNumber: String,
+    operator: String,
+    Latitude: Number,
+    Longitude: Number,
+    timePosition: Number,
+    Velocity: Number,
+    trueTrack: Number,
+    vertRate: Number,
+    geoAltitude: Number,
+    origin: String,
+    destination: String,
+})
 
 const Flight = mongoose.model('Flight', flightSchema)
 module.exports = Flight
---
+++ public/js/dbseeder.js
@@ -0,0 +1,17 @@
+// const axios = require('axios')
+//
+// axios({
+//     method: 'get',
+//     url:'https://opensky-network.org/api/states/all',
+//     responseType: 'json'
+// })
+//     .then(response => {
+//         for (const item in response.data.states){
+//
+//             console.log(response.data.states[item][1])
+//         }
+//
+//     })
+// .catch( () => {
+//     console.log('error')
+// })
