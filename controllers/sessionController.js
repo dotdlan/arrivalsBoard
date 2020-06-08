4e26e3ca72ca19f16c310c24ba55fd00f305828d
David Lance <david6116@yahoo.com>
2020-06-08 15:02:33 -0400

    user can now reg and log in from /

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const User = require('../models/users.js');
 const bcrypt = require('bcrypt');
 
-
-
 router.get('/new', (req, res) => {
     res.render('session/new.ejs');
 });
