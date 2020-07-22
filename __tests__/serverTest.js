972be8d3ae3335761536f4ef0d1e3274b5ac5461
David Lance <david6116@yahoo.com>
2020-07-21 22:41:05 -0400

    add a tich more testing, experiementing with jest

--- __tests__/serverTest.js
+++ __tests__/serverTest.js
@@ -1,7 +1,43 @@
-function sum(a, b) {
-    return a + b;
-}
+//supertest: https://www.npmjs.com/package/supertest
+//jest: https://jestjs.io/docs/en/getting-started.html
+//note the package.json config for mongodb apps: https://mongoosejs.com/docs/jest.html
+//supertest and express via "app"
+const request = require("supertest");
+const app = require("../server.js");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+//db conn stuff
+const mongoose = require('mongoose')
+
+//our .envstuff
+require('dotenv').config()
+
+
+beforeAll(async () => {
+    const MONGODB_URI = process.env.MONGODB_URI
+    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+})
+
+
+//test get on /
+describe("Test the root path", () => {
+    test("It should respond with a 302", done => {
+        request(app)
+            .get("/")
+            .then(response => {
+                expect(response.statusCode).toBe(302);
+                done();
+            });
+    });
+});
+
+//test get on /flight, our home page
+describe("Test flight path", () => {
+    test("It should respond with a 200", done => {
+        request(app)
+            .get("/flight")
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    });
 });
--- server.js
+++ server.js
@@ -57,3 +57,4 @@ app.listen(PORT, () => {
     console.log('Listening on port:', PORT)
 })
 
+module.exports = app;
